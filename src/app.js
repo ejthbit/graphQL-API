86d75389be28e83d50f82c901659f4ec844bb748
Filip Kotek <filip.kotek@compacer.com>
2020-06-25 12:43:52 +0200

    Added dotenv support

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 import {ApolloServer} from 'apollo-server-express'
 import {typeDefs} from './typeDefs/typedefs'
 import {resolvers} from './resolvers/resolvers'
+import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
 
-const PORT = 9000
-
 const startApolloServer = () => {
+    dotenv.config()
     const app = express()
     const server = new ApolloServer({typeDefs, resolvers})
     server.applyMiddleware({app})
-    mongoose.connect('mongodb://localhost:27017/person', {useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 
-    app.listen({port: PORT}, () => {
-        console.log(`🌑 Apollo Server is running at http://localhost:${PORT}/graphql`)
+    app.listen(process.env.RUNNING_PORT, () => {
+        console.log(`🚀🌑 Apollo Server is running at http://localhost:${process.env.RUNNING_PORT}/graphql`)
     })
 }
 startApolloServer()
